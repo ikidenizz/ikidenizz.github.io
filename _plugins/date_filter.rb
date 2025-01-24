60cd0d9e6787eb58e269bbdb2f34186030fd6cb5
isunion99 <union2030@proton.me>
2025-01-24 09:07:25 +0300

    commit

--- _plugins/date_filter.rb
+++ _plugins/date_filter.rb
@@ -2,10 +2,11 @@ module Jekyll
     module TurkishDateFilter
         def turkish_date(date)
             months = {
-                1 => "Ocak", 2 => "Şubat", 3 => "Mart", 4 => "Nisan", 5 => "Mayıs", 6 => "Haziran",
-                7 => "Temmuz", 8 => "Ağustos", 9 => "Eylül", 10 => "Ekim", 11 => "Kasım", 12 => "Aralık"
+                1 => "Ocak", 2 => "Şubat", 3 => "Mart", 4 => "Nisan",
+                5 => "Mayıs", 6 => "Haziran", 7 => "Temmuz", 8 => "Ağustos",
+                9 => "Eylül", 10 => "Ekim", 11 => "Kasım", 12 => "Aralık"
             }
-            "#{date.day} #{months[date.mon]} #{date.year}"
+            "#{date.day} #{months[date.month]} #{date.year}"
         end
     end
 end
